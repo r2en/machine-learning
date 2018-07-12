641f1448e343fbb966b63418a1ad570c666b1992
xxxHAL <s12161te@gmail.com>
2018-07-12 12:19:38 +0900

    add: readme

---
+++ keras/README.md
@@ -0,0 +1,5 @@
+# MNIST
+
+![figure_0](https://user-images.githubusercontent.com/17031124/42610683-a7643b4e-85cd-11e8-924c-577b630bf1ea.png)
+
+![figure_1](https://user-images.githubusercontent.com/17031124/42610684-a78a2caa-85cd-11e8-962f-8b52bb1ac114.png)
--- keras/multi-nuralnetwork.py
+++ keras/multi-nuralnetwork.py
@@ -1,70 +1,92 @@
-import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import RMSprop
-from keras.callbacks import EarlyStopping, CSVLogger
-import matplotlib.pyplot as plt
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+#from keras.utils.vis_utils import plot_model 
 
-batch_size = 128
-num_classes = 10
-epochs = 20
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+def build_multilayer_perceptron():
+    model = Sequential()
 
-# 28 * 28
-x_train = x_train.reshape(60000, 784).astype('float32')
-x_test = x_test.reshape(10000, 784).astype('float32')
-# Nomalization
-x_train /= 255
-x_test /= 255
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
 
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    return model
 
-model = Sequential()
-model.add(Dense(512, input_shape=(784, )))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10))
-model.add(Activation('softmax'))
 
-model.summary()
+def plot_history(history):
+    plt.plot(history.history['acc'], marker='.')
+    plt.plot(history.history['val_acc'], marker='.')
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.grid(
+    plt.legend(['acc', 'val_acc'], loc='lower right')
+    plt.show()
 
-model.compile(
-    loss='categorical_crossentropy',
-    optimizer=RMSprop(),
-    metrics=['accuracy']
-    )
+    plt.plot(history.history['loss'], marker='.')
+    plt.plot(history.history['val_loss'], marker='.')
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.grid()
+    plt.legend(['loss', 'val_loss'], loc='lower right')
+    plt.show()
 
-es = EarlyStopping(monitor='val_los', patience=2)
-csv_logger = CSVLogger('training.log')
-hist = model.fit(
-    x_train, y_train,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=1,
-    validation_split=0.1,
-    callbacks=[es, csv_logger]
-)
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('test loss: ', score[0])
-print('test acc: ', score[1])
+if __name__ == "__main__":
+    batch_size = 128
+    nb_classes = 10
+    nb_epoch = 100
 
-loss = hist.history['loss']
-val_loss = hist.history['val_loss']
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-epochs = len(loss)
-plt.plot(range(epochs), loss, marker='.', label='acc')
-plt.plot(range(epochs), val_loss, marker='.', label='val_acc')
-plt.legend(loc='best')
-plt.grid()
-plt.xlabel('epoch')
-plt.ylabel('acc')
-plt.show()
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
 
-import pdb ; pdb.set_trace()
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    model = build_multilayer_perceptron()
+
+    model.summary()
+    #plot_model(model, show_shapes=True, show_layer_names=True, to_file='model.png')
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
+
+    early_stopping = EarlyStopping(patience=0, verbose=1)
+
+    history = model.fit(X_train, Y_train,
+                        batch_size=batch_size,
+                        nb_epoch=nb_epoch,
+                        verbose=1,
+                        validation_split=0.1,
+                        callbacks=[early_stopping])
+
+    plot_history(history)
+
+    loss, acc = model.evaluate(X_test, Y_test, verbose=0)
+
+    print('Test loss:', loss)
+    print('Test acc:', acc)
